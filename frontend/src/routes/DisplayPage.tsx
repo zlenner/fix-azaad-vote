d93ceff62c11f20e0a4ed45aae4cb711255c3876
zlenner <119705166+zlenner@users.noreply.github.com>
2024-01-30 01:31:19 +0200

    image zoom

--- frontend/src/routes/DisplayPage.tsx
+++ frontend/src/routes/DisplayPage.tsx
@@ -1,58 +1,74 @@
-import { useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import { Constituency } from './models'
 import { RiArrowDropLeftLine, RiArrowDropRightLine } from 'react-icons/ri'
 import clsx from 'clsx'
+import ImageZoom from 'react-image-zoom'
 
 const DisplayPage = ({ constituency }: { constituency: Constituency }) => {
   const [selectedPage, setSelectedPage] = useState<number>(1)
+  const imageContainerRef = useRef<HTMLDivElement>(null)
+  const [imageHeight, setImageHeight] = useState<number>(0)
+
+  useEffect(() => {
+    setImageHeight(imageContainerRef.current?.clientHeight ?? 0)
+  }, [])
 
   return (
     <div className="flex flex-col h-full">
       <div className="bg-red-500 text-white px-4 py-2 font-bold">
         Scanned ECP-Form 33
       </div>
-      <img
-        className="w-full flex-1 h-0"
-        src={
-          'https://files.azaadvote.com/' +
-          constituency.PageFiles[selectedPage - 1]
-        }
-        style={{
-          objectFit: 'contain'
-        }}
-      ></img>
-      {constituency.PageFiles.length > 1 ? (
-        <div className="flex w-fit mb-4 mx-auto bg-gray-50 font-bold shadow rounded shadow-gray-300 px-3 py-2 relative bottom-6 items-center">
-          <RiArrowDropLeftLine
-            className={clsx(
-              'w-8 h-8 cursor-pointer hover:bg-gray-100 rounded active:bg-gray-200',
-              selectedPage === 1 && 'opacity-40 pointer-events-none'
-            )}
-            onClick={() => {
-              setSelectedPage((selectedPage) => Math.max(selectedPage - 1, 1))
-            }}
-          />
-          <div className="mx-4">
-            PAGE {selectedPage}/{constituency.PageFiles.length}
-          </div>
-          <RiArrowDropRightLine
-            className={clsx(
-              'w-8 h-8 cursor-pointer hover:bg-gray-100 rounded active:bg-gray-200',
-              selectedPage === constituency.PageFiles.length &&
-                'opacity-40 pointer-events-none'
-            )}
-            onClick={() => {
-              setSelectedPage((selectedPage) =>
-                Math.min(selectedPage + 1, constituency.PageFiles.length)
-              )
-            }}
+      <div className="flex flex-col w-full flex-1 h-0">
+        <div
+          className="flex flex-1 h-0 file-container justify-center"
+          ref={imageContainerRef}
+        >
+          <ImageZoom
+            img={
+              'https://files.azaadvote.com/' +
+              constituency.PageFiles[selectedPage - 1]
+            }
+            height={imageHeight}
+            zoomPosition="original"
           />
         </div>
-      ) : (
-        <div className="flex w-fit mb-4 mx-auto bg-gray-50 font-bold shadow rounded shadow-gray-300 px-3 py-2 relative bottom-6 items-center">
-          <div className="mx-2">PAGE 1/{constituency.PageFiles.length}</div>
+        <div className="flex flex-col">
+          {constituency.PageFiles.length > 1 ? (
+            <div className="flex w-fit mb-4 mx-auto bg-gray-50 font-bold shadow rounded shadow-gray-300 px-3 py-2 relative bottom-6 items-center">
+              <RiArrowDropLeftLine
+                className={clsx(
+                  'w-8 h-8 cursor-pointer hover:bg-gray-100 rounded active:bg-gray-200',
+                  selectedPage === 1 && 'opacity-40 pointer-events-none'
+                )}
+                onClick={() => {
+                  setSelectedPage((selectedPage) =>
+                    Math.max(selectedPage - 1, 1)
+                  )
+                }}
+              />
+              <div className="mx-4">
+                PAGE {selectedPage}/{constituency.PageFiles.length}
+              </div>
+              <RiArrowDropRightLine
+                className={clsx(
+                  'w-8 h-8 cursor-pointer hover:bg-gray-100 rounded active:bg-gray-200',
+                  selectedPage === constituency.PageFiles.length &&
+                    'opacity-40 pointer-events-none'
+                )}
+                onClick={() => {
+                  setSelectedPage((selectedPage) =>
+                    Math.min(selectedPage + 1, constituency.PageFiles.length)
+                  )
+                }}
+              />
+            </div>
+          ) : (
+            <div className="flex w-fit mb-4 mx-auto bg-gray-50 font-bold shadow rounded shadow-gray-300 px-3 py-2 relative bottom-6 items-center">
+              <div className="mx-2">PAGE 1/{constituency.PageFiles.length}</div>
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
   )
 }
